2431dac1cd4253b8f086954d00ea5991de0088d3
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2019-03-23 19:54:07 +0000

    Grammar fully anotated and LL(1)

--- src/ParserTreeConstants.java
+++ src/ParserTreeConstants.java
@@ -2,24 +2,82 @@
 public interface ParserTreeConstants
 {
   public int JJTID = 0;
-  public int JJTTYPE = 1;
-  public int JJTPROGRAM = 2;
-  public int JJTCLASS = 3;
-  public int JJTVAR = 4;
-  public int JJTMETHOD = 5;
-  public int JJTPARAMETERS = 6;
-  public int JJTVOID = 7;
+  public int JJTINTEGER = 1;
+  public int JJTVOID = 2;
+  public int JJTINTARRAY = 3;
+  public int JJTINT = 4;
+  public int JJTBOOLEAN = 5;
+  public int JJTAND = 6;
+  public int JJTLOWER = 7;
+  public int JJTPLUS = 8;
+  public int JJTMINUS = 9;
+  public int JJTTIMES = 10;
+  public int JJTDIVIDE = 11;
+  public int JJTPROGRAM = 12;
+  public int JJTCLASS = 13;
+  public int JJTVAR = 14;
+  public int JJTMETHOD = 15;
+  public int JJTPARAMETERS = 16;
+  public int JJTBODY = 17;
+  public int JJTRETURN = 18;
+  public int JJTASSIGN = 19;
+  public int JJTCOND = 20;
+  public int JJTTHEN = 21;
+  public int JJTELSE = 22;
+  public int JJTIF = 23;
+  public int JJTDO = 24;
+  public int JJTWHILE = 25;
+  public int JJTINDEX = 26;
+  public int JJTOP = 27;
+  public int JJTLENGTH = 28;
+  public int JJTFCALL = 29;
+  public int JJTTRUE = 30;
+  public int JJTFALSE = 31;
+  public int JJTTHIS = 32;
+  public int JJTNEWARRAY = 33;
+  public int JJTNEWOBJ = 34;
+  public int JJTNOT = 35;
+  public int JJTEXP = 36;
 
 
   public String[] jjtNodeName = {
     "ID",
-    "TYPE",
+    "INTEGER",
+    "void",
+    "INTARRAY",
+    "INT",
+    "BOOLEAN",
+    "AND",
+    "LOWER",
+    "PLUS",
+    "MINUS",
+    "TIMES",
+    "DIVIDE",
     "PROGRAM",
     "CLASS",
     "VAR",
     "METHOD",
     "PARAMETERS",
-    "void",
+    "BODY",
+    "RETURN",
+    "ASSIGN",
+    "COND",
+    "THEN",
+    "ELSE",
+    "IF",
+    "DO",
+    "WHILE",
+    "INDEX",
+    "OP",
+    "LENGTH",
+    "FCALL",
+    "TRUE",
+    "FALSE",
+    "THIS",
+    "NEWARRAY",
+    "NEWOBJ",
+    "NOT",
+    "EXP",
   };
 }
-/* JavaCC - OriginalChecksum=409cfaa0c8473f9566f5b9cd996a65a4 (do not edit this line) */
+/* JavaCC - OriginalChecksum=84ce1c37f096f33604673292d6a73bd0 (do not edit this line) */
