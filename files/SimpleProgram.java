735434629f1cd0d5c0086641ce3da73cc07baf69
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2019-04-10 10:09:45 +0100

    Some changes of w/e

--- files/SimpleProgram.java
+++ files/SimpleProgram.java
@@ -11,7 +11,7 @@ class Test extends Base {
 
         Test another_one;
         Base and_another_one;
-        num_aux = x + 5 * 4 / (2 + 3 / 4);
+        //num_aux = x + 5 * 4 / (2 + 3 / 4);
 
         //x = new Base().func((x - 1 + 3) * 4 + 3 / this.gimmeFive(), !check && false + new int[5].length);
         num_aux = x + 5;
@@ -21,28 +21,28 @@ class Test extends Base {
 
         Test.gimmeFive(x, redundant[this.gimmeFive()]);
 
-        num_aux = x + 5 * 4 / (2 + 3 / 4);
+        //num_aux = x + 5 * 4 / (2 + 3 / 4);
         if (check && another_redundant)
             num_aux = Test.gimmeFive();
         else {
-            num_aux = x + 5 * 4 / (2 + 3 / 4);
+            //num_aux = x + 5 * 4 / (2 + 3 / 4);
             while (num < 1) {
                x = x + 5;
-               num_aux = x + 5 * 4 / (2 + 3 / 4);            
+               //num_aux = x + 5 * 4 / (2 + 3 / 4);            
             }
 
             if (num < 1)
-                num_aux = x + 5 * 4 / (2 + 3 / 4);
+                num_aux = redundant[5].gimmeFive();//num_aux = x + 5 * 4 / (2 + 3 / 4);
             else
                 num_aux = x + 5 * 4 / (2 + 3 / 4);
             
             x = new Base().func((x + 3) * 4 + 3 / this.gimmeFive(), !check && false + new int[5].length);
         }
         
-        num_aux = x + 5 * 4 / (2 + 3 / 4);
+        //num_aux = x + 5 * 4 / (2 + 3 / 4);
         num_aux = x + 2 * 6;
 
-        return x + 5 * 4 / (2 + 3 / 4);
+        return x + 5 * 4 / (2 + redundant[3] / 4);
     }
 
     public int gimmeFive() {return 5;}
