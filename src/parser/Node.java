dd7d1e74fc9bdf88340505c73748f8ce955042f4
João Fernando da Costa Meireles Barbosa <joaofernandomeireles@gmail.com>
2019-06-07 20:22:20 +0100

    Added jarfile and README summary and execute tabs

--- src/parser/Node.java
+++ src/parser/Node.java
@@ -9,33 +9,33 @@ interface Node {
 
   /** This method is called after the node has been made the current
     node.  It indicates that child nodes can now be added to it. */
-  public void jjtOpen();
+  void jjtOpen();
 
   /** This method is called after all the child nodes have been
     added. */
-  public void jjtClose();
+  void jjtClose();
 
   /** This pair of methods are used to inform the node of its
     parent. */
-  public void jjtSetParent(Node n);
-  public Node jjtGetParent();
+  void jjtSetParent(Node n);
+  Node jjtGetParent();
 
-  public void jjtSetValue(Object value);
-  public Object jjtGetValue();
+  void jjtSetValue(Object value);
+  Object jjtGetValue();
 
-  public Token jjtGetFirstToken();
+  Token jjtGetFirstToken();
 
   /** This method tells the node to add its argument to the node's
     list of children.  */
-  public void jjtAddChild(Node n, int i);
+  void jjtAddChild(Node n, int i);
 
   /** This method returns a child node.  The children are numbered
      from zero, left to right. */
-  public Node jjtGetChild(int i);
+  Node jjtGetChild(int i);
 
   /** Return the number of children the node has. */
-  public int jjtGetNumChildren();
+  int jjtGetNumChildren();
 
-  public int getId();
+  int getId();
 }
 /* JavaCC - OriginalChecksum=a2ab0195dbe3c1dbb53c0c14bd211605 (do not edit this line) */
